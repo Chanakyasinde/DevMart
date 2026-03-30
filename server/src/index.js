c46a04240856503eb196369d1cc1f03af276f660
Chanakyasinde <chanakyasinde@gmail.com>
2026-03-30 14:40:42 +0530

    feat: added docker files

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app = require('./app');
 
 connectDB();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
